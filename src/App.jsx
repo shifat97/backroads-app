41227200abc61e2b4c6be133aa58a303d4ca7149
shifat97 <shifatbinreza2@gmail.com>
2025-05-07 18:08:16 +0600

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Services />
       <Tours />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import { pageLinks, socialLinks } from "../data";
+import SocialLink from "./SocialLink";
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links" id="footer-links">
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} className="footer-link">
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass="footer-icon" />;
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span>. all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
